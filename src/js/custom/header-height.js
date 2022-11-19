942d09ce35a305f1ee1acb0e3cdf5f6e61779518
yuutacn <yuutacn@gmail.com>
2022-11-19 15:05:28 +0300

    gulp-init

---
+++ src/js/custom/header-height.js
@@ -0,0 +1,6 @@
+// const getHeaderHeight = () => {
+//   const headerHeight = document?.querySelector('.header').offsetHeight;
+//   document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
+// }
+
+// getHeaderHeight()
